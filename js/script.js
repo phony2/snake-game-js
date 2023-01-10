7450d7a9c74f2e58ec034fa95a59e1e664e412dd
phony2 <122257891+phony2@users.noreply.github.com>
2023-01-10 10:21:57 +0400

    отрисовка игрового поля и еды

--- js/script.js
+++ js/script.js
@@ -15,6 +15,29 @@ const playingArea = new Image();
 playingArea.src = "img/area.png";
 
 /**
+* Создаем объект картинки и загружаем картинку еды
+*/
+const foodImage=new Image();
+foodImage.src="img/food.png";
+
+// скорость игры в милисекундах
+let speed=600;
+
+// размер одной клетки игрового поля
+let box=32;
+
+// позиция еды
+let food={x:5*box,y:5*box}
+
+/**
 * Функция для отрисовки игры
 */
-function drawGame() {}
+function drawGame() {
+	//отрисовка фона игрового поля
+	ctx.drawImage(playingArea,0,0);
+	
+	// отрисовка еды
+	ctx.drawImage(foodImage,food.x,food.y);
+}
+
+let game=setInterval(drawGame,speed);
